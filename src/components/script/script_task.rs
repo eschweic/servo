adf3485db5154a11ae264aec4aa3cc73c530febd
Tim Kuehn <tkuehn@cmu.edu>
2013-08-07 17:52:09 -0700

    refactor iframe element fields to support sending size to the constellation

--- src/components/script/script_task.rs
+++ src/components/script/script_task.rs
@@ -605,7 +605,8 @@ impl ScriptTask {
                                                                  url.clone(),
                                                                  self.resource_task.clone(),
                                                                  self.image_cache_task.clone(),
-                                                                 page.next_subpage_id.clone());
+                                                                 page.next_subpage_id.clone(),
+                                                                 self.constellation_chan.clone());
 
         let HtmlParserResult {root, js_port, style_port, iframe_port} = html_parsing_result;
 
